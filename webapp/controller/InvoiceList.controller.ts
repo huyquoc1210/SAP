495761dca93eab5ef547d72abc4a1257f4855368
huyquoc1210 <contact.huyquoc@gmail.com>
2024-12-09 16:21:59 +0700

    Filtering

--- webapp/controller/InvoiceList.controller.ts
+++ webapp/controller/InvoiceList.controller.ts
@@ -1,5 +1,9 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import { SearchField$SearchEvent } from "sap/m/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
 
 /**
  * @namespace ui5.walkthrough.controller
@@ -10,5 +14,19 @@ export default class App extends Controller {
       currency: "EUR",
     });
     this.getView()?.setModel(viewModel, "view");
+  }
+
+  onFilterInvoices(event: SearchField$SearchEvent): void {
+    // build filter array
+    const filter = [];
+    const query = event.getParameter("query");
+    console.log(query);
+    if (query) {
+      filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+    }
+    // filter binding
+    const list = this.byId("invoiceList");
+    const binding = list?.getBinding("items") as ListBinding;
+    binding?.filter(filter);
   }
 }
